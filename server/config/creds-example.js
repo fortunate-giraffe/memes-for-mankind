5c403f484bf911f356a91f5cbb10a3c72d0ea6dc
i-made-that <rogerthomasson@gmail.com>
2015-04-03 20:00:13 -0700

    Created Memes routes and Controller

---
+++ server/config/creds-example.js
@@ -0,0 +1,9 @@
+/*
+This is an example file for how the memegenerator credentials should be structured.
+After you add your credentials, change the filename to creds.js and it will be ignored.
+ */
+
+module.exports = {
+  username: APIusernameH,
+  password: APIpassword
+};
--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,3 +1,13 @@
+var bodyParser = require('body-parser');
+var memeController = require('../memes/memeController.js');
+
 module.exports = function (app, express) {
+
   app.use(express.static(__dirname + '/../../client'));
+
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+
+  app.get('/memes', memeController.getMemes);
+  app.get('/memes/create', memeController.createMeme);
 };
