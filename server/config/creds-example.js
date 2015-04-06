138fc35869e7de4b312ea620f64e96461f83c8db
i-made-that <rogerthomasson@gmail.com>
2015-04-06 16:23:02 -0700

    Code cleanup

--- server/config/creds-example.js
+++ server/config/creds-example.js
@@ -1,7 +1,7 @@
 /*
 This is an example file for how the memegenerator credentials should be structured.
 After you add your credentials, change the filename to creds.js and it will be ignored.
- */
+*/
 
 module.exports = {
   username: APIusername,
