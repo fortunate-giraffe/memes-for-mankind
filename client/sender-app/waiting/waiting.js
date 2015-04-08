1e7778a87e57f65e105839690cebe6d14c9d9df7
Rich Magee <rmagee88@gmail.com>
2015-04-08 09:50:39 -0700

    (feat) Implemented listeners for start/waiting and updated middleware/index for core deps

--- client/sender-app/waiting/waiting.js
+++ client/sender-app/waiting/waiting.js
@@ -5,13 +5,39 @@
       .module('app.waiting')
       .controller('Waiting', Waiting);
 
-  Waiting.$inject = ['simpleObj'];
+  Waiting.$inject = ['simpleObj', 'playerMessenger', 'playerUser', '$state'];
 
-  function Waiting(simpleObj) {
+  function Waiting(simpleObj, playerMessenger, playerUser, $state) {
     /*jshint validthis: true */
     var vm = this;
     vm.test = 'Waiting';
     vm.simple = simpleObj;
     toastr.info(simpleObj.value);
+    toastr.info(playerUser.getUser());
+
+    changeStateListener();
+
+    // listener for msg to indicate what's next
+    function changeStateListener() {
+      // on promptSubmitted msg from Chromecast, everyone but the judge to choosing page
+      playerMessenger.on('promptSubmitted', function() {
+        if (playerUser.getRole() !== 'judge') {
+          $state.go('home.choosing');
+        }
+      });
+
+      // on startJudging msg from Chromecast, judge goes to the choosing page, everyone else stays
+      playerMessenger.on('startJudging', function() {
+        // if role is judge, go to prompt
+        if (playerUser.getRole() === 'judge') {
+          $state.go('home.choosing');
+        }
+      });
+
+      // on done msg from Chromecast, go to start page
+      playerMessenger.on('done', function() {
+        $state.go('home.start');
+      });
+    }
   }
 })();
