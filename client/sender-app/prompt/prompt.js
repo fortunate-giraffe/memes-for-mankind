1a83d59cb3573de61c23743f315bd219353bfd21
i-made-that <rogerthomasson@gmail.com>
2015-04-08 11:00:01 -0700

    (feat) Added handler/listener for prompt.js

--- client/sender-app/prompt/prompt.js
+++ client/sender-app/prompt/prompt.js
@@ -5,17 +5,30 @@
       .module('app.prompt')
       .controller('Prompt', Prompt);
 
-  Prompt.$inject = [];
+  Prompt.$inject = ['playerMessenger','$state'];
 
-  function Prompt() {
+  function Prompt(playerMessenger, $state) {
     /*jshint validthis: true */
     var vm = this;
     vm.prompt = '';
     vm.submitPrompt = submitPrompt;
 
+    // establishing listener
+    changeStateListener();
+
     // example to handle prompt submission
     function submitPrompt() {
+      var prompt = {
+        prompt: vm.prompt
+      };
+      playerMessenger.submit(prompt);
       toastr.info('submitted this prompt: ' + vm.prompt);
+    }
+
+    function changeStateListener(){
+      playerMessenger.on('promptSubmitted', function(){
+        $state.go('home.waiting');
+      });
     }
   }
 })();
