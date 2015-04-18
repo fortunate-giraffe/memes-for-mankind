05b70aa27be88e7fe3bfbfe190f684493fb7fc84
Rich Magee <rmagee88@gmail.com>
2015-04-18 13:58:45 -0700

    (feat) added new curated memes to database and corresponding mongoose model

--- server/memes/memeModel.js
+++ server/memes/memeModel.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose');
 var random = require('mongoose-random');
 var Schema = mongoose.Schema;
 
+var schemasObj = {};
+
 var MemeSchema = new Schema({
   generatorID: Number,
   displayName: String,
@@ -15,8 +17,29 @@ var MemeSchema = new Schema({
   context: String
 });
 
-MemeSchema.plugin(random, { path: 'r' });
+var CuratedMemeSchema = new Schema({
+  gen_id: Number, // jshint ignore:line
+  displayName: String,
+  imageUrl: String,
+  context: String
+});
 
-module.exports = mongoose.model('Meme', MemeSchema);
+MemeSchema.plugin(random, { path: 'r' });
+CuratedMemeSchema.plugin(random, { path: 'r' });
+
+schemasObj.memes = mongoose.model('Meme', MemeSchema);
+schemasObj.curatedmemes = mongoose.model('curatedMeme', CuratedMemeSchema);
+
+for (var key in schemasObj) {
+  schemasObj[key].syncRandom(function (err) { // result
+    if(err) {console.log('schema err', err);}
+    console.log('random sync of schema complete');
+  }); // jshint ignore:line
+}
+
+module.exports = {
+  memes: schemasObj.memes,
+  curatedmemes: schemasObj.curatedmemes
+};
 
 
