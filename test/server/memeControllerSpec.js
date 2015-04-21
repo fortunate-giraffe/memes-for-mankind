a924eec58bfcfa28df40b1791bcf1751085549e7
i-made-that <rogerthomasson@gmail.com>
2015-04-20 19:41:54 -0700

    (cleanup) Remove apiCalls.js

---
+++ test/server/memeControllerSpec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var assert = require('assert');
+var memeController = require('../../server/memes/memeController.js');
+
+describe('memeController', function(){
+
+    it('should have a getMemes method', function(){
+      expect(memeController.hasOwnProperty('getMemes')).toEqual(true);
+    });
+
+    it('should have a createMeme method', function(){
+      expect(memeController.hasOwnProperty('createMeme')).toEqual(true);
+    });
+});
