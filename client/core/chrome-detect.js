b38742f36920e8ec915afcc7e6aead203152f9b7
Rich Magee <rmagee88@gmail.com>
2015-04-23 18:51:09 -0700

    (fix) Mobile sharing, just needed a little plug in love

--- client/core/chrome-detect.js
+++ client/core/chrome-detect.js
@@ -18,7 +18,7 @@
     function checkBrowser () {
       // if the user doesn't have chrome at all, tell them to get Chrome and the cast extension
       // only actually check if they also don't have cordova and haven't loaded up the socket version
-      browserIsChrome = (!window.cordova && !window.chrome && !socketDev) ? false : true;
+      browserIsChrome = (window.cordova || window.chrome || socketDev);
       return browserIsChrome;
     }
 
@@ -26,8 +26,7 @@
       // if the user doesn't have the chrome extension, tell them to get the cast extension
       // only actually check if they also don't have cordova and haven't loaded up the socket version
       // check browser first to avoid getting a null reference on cast.extensionId
-      browserHasExtension = (!window.cordova && !window.chrome.cast.extensionId && !socketDev) ? false : true;
-
+      browserHasExtension = (window.cordova || window.chrome.cast.extensionId || socketDev);
       return browserHasExtension;
     }
   }
--- client/sender-app/done/done.js
+++ client/sender-app/done/done.js
@@ -10,9 +10,17 @@
   function Done(playerMessenger, playerUser) {
     /*jshint validthis: true */
     var vm = this;
+    vm.cordova = (window.cordova) ? true : false;
 
     vm.winningMeme = playerUser.getWinner();
 
     vm.startNextRound = playerMessenger.startNextRound;
+
+    vm.mobileShare = mobileShare;
+
+    function mobileShare() {
+      console.log('mobileshare called!');
+      window.plugins.socialsharing.share('Check out this Meme we made playing MemesForMankind!', null, null, vm.winningMeme.meme.instanceImageUrl);
+    }
   }
 })();
